8d935aaf42da6d7b5168534185adf16d2226b26b
Bryan Tumbaco <bryandario1994@gmail.com>
2020-10-08 12:23:08 -0500

    add build

---
+++ docs/precache-manifest.e6071484c6e86a305a355d6d66d594c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a06a2d3e3848becd408f0c898e357d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4f83ca6cfc945365c48",
+    "url": "/static/css/main.9b8dd3bd.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4f83ca6cfc945365c48",
+    "url": "/static/js/main.e63b2a1b.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
